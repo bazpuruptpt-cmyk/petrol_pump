fb21fefd58ad7ed110b9f0b467c1cc0c8fbb6181
bazpuruptpt-cmyk <bazpuruptpt@gmail.com>
2026-05-17 22:21:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -71,7 +71,6 @@ OWNER_NAV_GROUPS = {
     },
     "Creditors": {
         "Credit Parties": credit_parties_page,
-        "Credit Approval / WhatsApp Logs": credit_approval_page,
         "Credit Payment": credit_payment_page,
     },
     "Stock": {
@@ -102,7 +101,6 @@ MANAGER_NAV_GROUPS = {
     },
     "Creditors": {
         "Credit Parties": credit_parties_page,
-        "Credit Approval / WhatsApp Logs": credit_approval_page,
         "Credit Payment": credit_payment_page,
     },
     "Stock": {
